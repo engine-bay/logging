1cc7bbb233c65699e52ecb78849cd6b833a78d68
Janus Liebenberg <janus.liebenberg@entelect.co.uk>
2024-01-09 15:40:06 +0000

    #patch: switch to floating versions for core

--- EngineBay.Logging/Constants/EnvironmentVariableConstants.cs
+++ EngineBay.Logging/Constants/EnvironmentVariableConstants.cs
@@ -2,7 +2,6 @@ namespace EngineBay.Logging
 {
     public static class EnvironmentVariableConstants
     {
-        public const string LOGGINGSENSITIVEDATAENABLED = "LOGGING_SENSITIVE_DATA_ENABLED";
         public const string LOGGINGLEVEL = "LOGGING_LEVEL";
     }
 }
